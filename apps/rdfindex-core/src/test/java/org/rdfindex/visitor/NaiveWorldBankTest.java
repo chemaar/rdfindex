23d64710f563751198a5eafc4494b2f4de7744ea
Jose María Alvarez <jmalvarez@seerc.org>
2013-06-21 09:59:45 +0300

    Adding new test

--- apps/rdfindex-core/src/test/java/org/rdfindex/visitor/NaiveWorldBankTest.java
+++ apps/rdfindex-core/src/test/java/org/rdfindex/visitor/NaiveWorldBankTest.java
@@ -10,13 +10,16 @@ import org.junit.Test;
 import org.rdfindex.dao.MetadataDAOImpl;
 import org.rdfindex.dao.RDFIndexMetadataDAO;
 import org.rdfindex.to.IndexTO;
+import org.rdfindex.to.ObservationTO;
+import org.rdfindex.utils.PrettyPrinter;
+import org.rdfindex.utils.SPARQLQueriesHelper;
 
 import test.utils.TestHelper;
 
 public class NaiveWorldBankTest {
 
 	@Test
-	public void testWeightedQueryIndex() throws Exception {
+	public void testSPARQLVisitor() throws Exception {
 		RDFIndexMetadataDAO metadata = new MetadataDAOImpl(
 				TestHelper.INDEX_MODEL, 
 				TestHelper.createModel("wb/naive-worldbank.ttl"),
@@ -29,9 +32,28 @@ public class NaiveWorldBankTest {
 			String sparqlQuery = (String) rdfIndexProcessor.visit(index);			
 			Assert.assertEquals(3544 , sparqlQuery.length());
 			//PrettyPrinter.prettyPrint(SPARQLQueriesHelper.observationsAsRDF(indexObservations));
-		
 			
 		}
+	}
+	
+	@Test
+	public void testProces() throws Exception {
+		RDFIndexMetadataDAO metadata = new MetadataDAOImpl(
+				TestHelper.INDEX_MODEL, 
+				TestHelper.createModel("wb/naive-worldbank.ttl"),
+				TestHelper.createModel("wb/naive-worldbank-observations.ttl"));	
+		
+		RDFIndexVisitor rdfIndexProcessor = new RDFIndexProcessorVisitor(metadata);
+		//Test as visitor
+		List<IndexTO> indexes = metadata.getIndexMetadata();
+		
+		for(IndexTO index:indexes){
+			List<ObservationTO> indexObservations = (List<ObservationTO>) rdfIndexProcessor.visit(index);
+			PrettyPrinter.prettyPrint(SPARQLQueriesHelper.observationsAsRDF(indexObservations));
+			Assert.assertEquals(4, indexObservations.size());
+			
+		}
+
 	}
 
 }
